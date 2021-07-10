18b7096fe4ddcb58aa81372ebc660592b85dd11a
Zyro <38949958+0Zyro@users.noreply.github.com>
2021-07-10 16:41:02 -0300

    Basic working bot
    basico discord bot with ping command

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const { Client } = require ("discord.js");
+const client = new Client();
+
+client.login("ODYzMjYxMDg1OTIxMTgxNzM2.YOkUiQ.mNcmHYeWvx4GR_KTwwzXt_6VtN8");
+
+//este evento se ejecuta cuando el bot ya esta funcionando al iniciar
+client.on("ready", () => {
+
+    //imprime este mensaje por consola
+    console.log("bot ready");
+})
+
+//al llegar un mensaje a un canal de chat se ejecuta este evento
+client.on("message", msg => {
+
+    //si el mensaje es "!ping", el bot respondera @usuario, pong
+    //al parecer msg.reply etiqueta al usuario, pone una coma y el mensaje especificado
+    if(msg.content === "!ping")
+    {
+        msg.reply("pong");
+    }
+});
