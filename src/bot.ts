affc10927de85883ed36a3d60763a269bb7ff6ce
Timothy Ramallo <timo.22394@hotmail.com>
2022-06-12 17:09:51 -0300

    created todos file | moved general todos from bot.ts to todos file | improved todo on bot.ts file

--- src/bot.ts
+++ src/bot.ts
@@ -3,19 +3,9 @@ import { BOT_TOKEN } from './secrets.json'
 import ready from './listeners/ready'
 import interactionCreate from './listeners/interactionCreate'
 
-// TODO: dig into TODO Tree plugin docs to figure out a configuration to use in it
-
-// TODO: investigate a proper way to deal with secret values, move secrets.json somewhere else
-
-// TODO: investigate license types
-
-// TODO: fill up the package.json file
-
-// TODO: fill up the README.md file
-
 console.log('Bot is starting...')
 
-// TODO: read intents page & configure the bot to use the needed ones only
+// TODO: read 'intents' page & configure the bot to use the needed ones only
 const allIntents = new Intents(32767)
 
 const client = new Client({
