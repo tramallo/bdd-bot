b432ccd0d14fea30a0e93ed7833f1cf4c6b969d9
Timothy Ramallo <Timothy.Ramallo@endava.com>
2022-09-01 19:32:47 -0300

    behaviour definition improved

--- src/behaviours/ping.behaviour.ts
+++ src/behaviours/ping.behaviour.ts
@@ -1,21 +1,24 @@
-import { CommandInteraction, Interaction } from 'discord.js'
+import { CommandInteraction } from 'discord.js'
 import { Bot } from '../common/bot.class'
 import { Behaviour } from '../common/behaviour.class'
+import { Command } from '../common/types'
+
+const pingCommand = () =>
+    Promise.resolve({
+        name: 'ping',
+        description: 'replies pong!',
+        onCall: async (interaction: CommandInteraction): Promise<void> => {
+            //await interaction.deferReply()
+            await interaction.reply('pong!')
+            return
+        },
+    } as Command)
 
 export default async (bot: Bot): Promise<Behaviour> => {
-    const ping = new Behaviour('ping')
-
-    ping.addCommand((bot) =>
-        Promise.resolve({
-            name: 'ping',
-            description: 'replies pong!',
-            onCall: async (interaction: CommandInteraction): Promise<void> => {
-                //await interaction.deferReply()
-                await interaction.reply('pong!')
-                return
-            },
-        }),
-    )
+    const ping = new Behaviour({
+        name: 'ping',
+        commands: [pingCommand],
+    })
 
     return ping
 }
